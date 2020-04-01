62574f585b61fbc0a2880e8896b2eb64d20f9c0c
etotsamyi <kindcat1101@gmail.com>
2020-04-01 03:06:15 +0300

    Updates

---
+++ precache-manifest.715f32a8869c02984243cd00cc23c8b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1535b10b9a15dd67a09dfeeb491fce11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4d376fd3ed32286892a",
+    "url": "/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "5e9d01c2d4ca0067a7d5",
+    "url": "/static/css/main.6e17c647.chunk.css"
+  },
+  {
+    "revision": "f4d376fd3ed32286892a",
+    "url": "/static/js/2.5544b5e9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5544b5e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e9d01c2d4ca0067a7d5",
+    "url": "/static/js/main.7a18f275.chunk.js"
+  },
+  {
+    "revision": "08f1d0745b86d71c4472",
+    "url": "/static/js/runtime-main.3901ef34.js"
+  }
+]);
